df029bc782bcd1b65cfdf7fb9cf9c4a1045db7cb
Santiago Barrera <santiago.barrerae@gmail.com>
2022-03-30 11:32:00 -0500

    Dependencias y configuraciones iniciales

---
+++ src/main/java/co/com/donraul/app/ServletInitializer.java
@@ -0,0 +1,13 @@
+package co.com.donraul.app;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(AppApplication.class);
+	}
+
+}
