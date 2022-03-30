73a30b4adaa2c32a52843981c641a84ad0a35be0
Santiago Barrera <santiago.barrerae@gmail.com>
2022-03-30 11:58:13 -0500

    modelo proveedor

---
+++ src/main/java/co/com/donraul/app/models/Proveedor.java
@@ -0,0 +1,65 @@
+package co.com.donraul.app.models;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "proveedores")
+public class Proveedor {
+
+    @Id
+    private String id;
+    private String nombre;
+    private String celular;
+    private String numIdentificacion;
+
+    public Proveedor() {
+    }
+
+    public Proveedor(String nombre, String celular, String numIdentificacion) {
+        this.nombre = nombre;
+        this.celular = celular;
+        this.numIdentificacion = numIdentificacion;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+
+    public String getNumIdentificacion() {
+        return numIdentificacion;
+    }
+
+    public void setNumIdentificacion(String numIdentificacion) {
+        this.numIdentificacion = numIdentificacion;
+    }
+
+    @Override
+    public String toString() {
+        return "Proveedor{" +
+                "id='" + id + '\'' +
+                ", nombre='" + nombre + '\'' +
+                ", celular='" + celular + '\'' +
+                ", numIdentificacion='" + numIdentificacion + '\'' +
+                '}';
+    }
+}
