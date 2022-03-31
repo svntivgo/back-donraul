7f276786f326eb6c363502b9b22b1ccbe9dfb148
Santiago Barrera <santiago.barrerae@gmail.com>
2022-03-31 02:58:44 -0500

    swagger

--- src/main/java/co/com/donraul/app/AppApplication.java
+++ src/main/java/co/com/donraul/app/AppApplication.java
@@ -2,10 +2,27 @@ package co.com.donraul.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableReactiveMongoRepositories
+@EnableSwagger2
 public class AppApplication {
 
+	@Bean
+	public Docket citasApi(){
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.any())
+				.paths(PathSelectors.any())
+				.build();
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(AppApplication.class, args);
 	}
