d15fb5c56a655ec42c27c9aa10082852729542ae
Santiago Barrera <santiago.barrerae@gmail.com>
2022-03-31 18:30:22 -0500

    cross origin

--- src/main/java/co/com/donraul/app/controllers/FacturaController.java
+++ src/main/java/co/com/donraul/app/controllers/FacturaController.java
@@ -12,6 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:3000")
 public class FacturaController {
 
     @Autowired
--- src/main/java/co/com/donraul/app/controllers/VolanteController.java
+++ src/main/java/co/com/donraul/app/controllers/VolanteController.java
@@ -12,6 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:3000")
 public class VolanteController {
 
     @Autowired
