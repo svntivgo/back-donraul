bb714dbd066228cec3e3b001c9ac4726a1fc63c9
Santiago Barrera <santiago.barrerae@gmail.com>
2022-03-31 00:47:34 -0500

    buscar por id y buscar todos controlador para volantes

--- src/main/java/co/com/donraul/app/controllers/VolanteController.java
+++ src/main/java/co/com/donraul/app/controllers/VolanteController.java
@@ -5,6 +5,7 @@ import co.com.donraul.app.models.Volante;
 import co.com.donraul.app.services.VolanteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.util.List;
@@ -19,5 +20,15 @@ public class VolanteController {
     @PostMapping("/volante/")
     private Mono<Volante> agregar (@RequestParam("numIdentification") String numIdentification, @RequestBody List<Producto> productos) {
         return volanteService.agregarVolante(numIdentification,productos);
+    }
+
+    @GetMapping("/volante/")
+    private Mono<Volante> buscarPorId (@RequestParam("id") String id) {
+        return volanteService.buscarVolantePorId(id);
+    }
+
+    @GetMapping("/volantes")
+    private Flux<Volante> buscarTodo () {
+        return volanteService.buscarVolantes();
     }
 }
