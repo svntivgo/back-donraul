3525845f2e17c27ad50fafb8541e06b78850cef5
Santiago Barrera <santiago.barrerae@gmail.com>
2022-03-30 20:09:42 -0500

    repositorio para volante

---
+++ src/main/java/co/com/donraul/app/repositories/VolanteRepository.java
@@ -0,0 +1,6 @@
+package co.com.donraul.app.repositories;
+
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+
+public interface VolanteRepository extends ReactiveMongoRepository {
+}
