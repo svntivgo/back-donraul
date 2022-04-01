9e2f30e7986c4245eb5a4d9e71c6cff42ffdc927
Santiago Barrera <santiago.barrerae@gmail.com>
2022-04-01 17:17:11 -0500

    se modifican dependencias

--- src/main/java/co/com/donraul/app/AppApplication.java
+++ src/main/java/co/com/donraul/app/AppApplication.java
@@ -16,7 +16,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class AppApplication {
 
 	@Bean
-	public Docket citasApi(){
+	public Docket donraulApi() {
 		return new Docket(DocumentationType.SWAGGER_2)
 				.select()
 				.apis(RequestHandlerSelectors.any())
