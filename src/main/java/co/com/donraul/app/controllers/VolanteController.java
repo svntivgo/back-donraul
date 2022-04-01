063cb3e9679ac234129b5f36da8c68b222db614d
Santiago Barrera <santiago.barrerae@gmail.com>
2022-04-01 17:46:15 -0500

    crossorigin localhost

--- src/main/java/co/com/donraul/app/controllers/VolanteController.java
+++ src/main/java/co/com/donraul/app/controllers/VolanteController.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api")
-//@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "http://localhost:3000")
 public class VolanteController {
 
     @Autowired
