fb2d308329f06fd94becee08d9a562006aad0ae9
Santiago Barrera <santiago.barrerae@gmail.com>
2022-04-01 16:55:19 -0500

    servletinit

--- src/main/java/co/com/donraul/app/ServletInitializer.java
+++ src/main/java/co/com/donraul/app/ServletInitializer.java
@@ -3,11 +3,11 @@ package co.com.donraul.app;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-public class ServletInitializer extends SpringBootServletInitializer {
-
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(AppApplication.class);
-	}
-
-}
+//public class ServletInitializer extends SpringBootServletInitializer {
+//
+//	@Override
+//	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+//		return application.sources(AppApplication.class);
+//	}
+//
+//}
