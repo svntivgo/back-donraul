8b41b58b90fea9fc3a34c96a43cafbfb02dee786
Santiago Barrera <santiago.barrerae@gmail.com>
2022-04-01 17:24:32 -0500

    se modifican dependencias

--- src/main/java/co/com/donraul/app/AppApplication.java
+++ src/main/java/co/com/donraul/app/AppApplication.java
@@ -11,7 +11,6 @@ import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EnableReactiveMongoRepositories
 @EnableSwagger2
 public class AppApplication {
 
